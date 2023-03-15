3caf8f4a93aacd0763effda7eeb6061d512def8c
sakimotorin <sakimotorin@gmail.com>
2023-03-15 17:31:10 +0800

    deploy backend page - product page and order page

---
+++ assets/AboutView-9e628140.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o}from"./index-7bd3767d.js";const c={},s={class:"text-center text-danger"};function n(r,a,_,p,d,f){return o(),e("h2",s,"About")}const u=t(c,[["render",n]]);export{u as default};
---
+++ assets/HomeView-62571dd0.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index-7bd3767d.js";const c={},s={class:"text-center text-danger"};function n(r,a,_,p,d,f){return o(),t("h2",s,"Home")}const m=e(c,[["render",n]]);export{m as default};
